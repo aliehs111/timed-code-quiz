b5c04ebdf1f39862aac23d1ae4ef5099919b711a
aliehs111 <128503077+aliehs111@users.noreply.github.com>
2023-06-07 20:38:09 -0400

    html and css for first two divs

---
+++ script.js
@@ -0,0 +1,5 @@
+let startBtn = document.querySelector('#start')
+startBtn.addEventListener('click', function(){
+    document.querySelector('.container').style.display='none'
+    document.querySelector('.quiz-container').style.display='block'
+})
