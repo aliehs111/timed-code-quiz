041d60675fc38189c9e0022d01874910e3b8e94a
aliehs111 <128503077+aliehs111@users.noreply.github.com>
2023-06-10 14:19:39 -0400

    changed class names for question buttons and added divs for scores

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-let startBtn = document.querySelector('#start')
+
+
+
+
+
+
+
+let startBtn = document.querySelector('#start');
 startBtn.addEventListener('click', function(){
     document.querySelector('.container').style.display='none'
+    document.querySelector('.finito').style.display='none'
+    document.querySelector('.highscores').style.display='none'
     document.querySelector('.quiz-container').style.display='block'
+})
+
+// const correctArr = ['method', 'object', 'html', 'javascript'];
+let userAnswer = [];
+
+let questcontainer = document.querySelector('.questcontainer')
+
+questcontainer.addEventListener("click", function(event) {
+    event.stopPropagation();
+    var element = event.target;
+    console.log(element.classList)
+    if(element.classList.contains("answopt-cor")) {
+        console.log("1")
+        else(console.log("0"))
+    }
+
 })
