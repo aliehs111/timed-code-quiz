e6fbc8716136d244eee4e8aace5469375869818c
aliehs111 <128503077+aliehs111@users.noreply.github.com>
2023-06-10 20:13:40 -0400

    revised functions and question array

--- script.js
+++ script.js
@@ -5,82 +5,58 @@ startBtn.addEventListener('click', function(){
     document.querySelector('.finito').style.display='none'
     document.querySelector('.highscores').style.display='none'
     document.querySelector('.quiz-container').style.display='block'
+
 })
 
 //quiz time and score variables
 let thisQuestionInd = 0
-let time = questions.length * 15;
+// let time = questions.length * 15;
 // let timerID;
 
 let questionEl = document.getElementById('question');
 let answOption = document.getElementById('options');
-let timeEl = 
+let timeEl = document.getElementById('time');
 
-let quiz-questions = [ 
-{
+let quizQuestions = [ 
+{   numb: 1,
     h1says: 'A group of key/value pairs is called a(n) _____',
     options: ['function', 'array', 'method', 'object'],
     answer: 'object',
 },
 
 {
+    numb: 2,
     h1says: 'In a function, the keyword "this" refers to a(n)______',
     options: ['function', 'array', 'method', 'object'],
     answer: 'object',
 },
 
 {
+    numb: 3,
     h1says: 'An object property containing a definition is a _____',
     options: ['function', 'array', 'method', 'object'],
     answer: 'method',
 },
 
 {
+    numb: 4,
     h1says: 'This is used to store multiple values in a single variable',
     options: ['function', 'array', 'method', 'object'],
     answer: 'array',
 },
 
 {
+    numb: 5,
     h1says: 'A parameter is a named variable passed into a ______',
     options: ['function', 'array', 'method', 'object'],
     answer: 'function',
 },
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ]
-// const correctArr = ['method', 'object', 'html', 'javascript'];
-let userAnswer = [];
 
-let questcontainer = document.querySelector('.questcontainer')
-let correctAnswer = document.querySelector('.answopt-cor')
-let incorrectAnser = document.querySelector('.answer')
 
-// questcontainer.addEventListener("click", function(event) {
-    correctAnswer.addEventListener("click", function(event) {
-    event.stopPropagation();
-    var element = event.target;
-    console.log(element.classList)
-    if(element.classList.contains("answopt-cor")) {
-        console.log("1")
-        console.log(userAnswer.push)
-        document.querySelector('#correct').style.display='block'
-  
-    }
+function nextQuestion(){
+    quizQuestions
+}
 
-})
+
